3b4a44507ea60dc3834fdf8d1b39e6207cf7d803
olicheng <olicheng@student.ubc.ca>
2024-03-27 22:43:41 -0700

    Started working on Test Suite

--- test/GuessScoreTest.py
+++ test/GuessScoreTest.py
@@ -10,10 +10,11 @@ class TestGuessScore(unittest.TestCase):
         self.game = GuessScore()
 
     def test_correct_guess(self):
-        self.assertEqual(self.game.score_guess("python", "python"), 10, "The score should be 10 for a correct guess")
+        self.assertTrue(self.game.score_guess("python", "python"), "The score should be 10 for a correct guess")
 
     def test_incorrect_guess(self):
-        self.assertEqual(self.game.score_guess("python", "java"), 0, "The score should be 0 for an incorrect guess")
+        self.assertTrue(self.game.score_guess("python", "java"), "The score should be 0 for an incorrect guess")
+
 
 if __name__ == '__main__':
     unittest.main()
